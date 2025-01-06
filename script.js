1957b414747e95a08f97f43aa301ac874def5ce6
faizsheikh1608 <164844564+faizsheikh1608@users.noreply.github.com>
2025-01-07 00:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -25,10 +25,7 @@ SportsCar.prototype.getTopSpeed = function () {
   return this.topSpeed;
 };
 
-// Example usage
-const car = new SportsCar("Ferrari", "Testarossa", 200);
-console.log(car.getMakeModel()); // Output: Ferrari Testarossa
-console.log(car.getTopSpeed()); // Output: 200
+
 
 // Do not change the code below
 window.Car = Car;
